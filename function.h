59796be3dacc722bc4a1ba00ff699cbb3d8149c7
tataglarasati11 <60909380+tataglarasati11@users.noreply.github.com>
2020-04-27 10:45:19 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+class bangun
+{
+ protected:
+       double x,y,z;
+       
+  public:
+       virtual void get_data()=0;
+       virtual void display_area()=0;
+};
+
+class segitiga: public bangun
+{
+   public:
+    void get_data(void)
+    {
+	   cout<<"Menghitung panjang sisi dengan pytagoras \n\n";
+       cout<<"masukkan nilai a = "; cin>>x;
+       cout<<"masukkan nilai b = "; cin>>y;
+    }
+    void display_area(void)
+    {
+       double pytagoras;
+       pytagoras =sqrt(pow(x,2)+pow(y,2));
+       cout<<"nilai c adalah = "<<pytagoras<<"\n";
+    }
+};
+
---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#include "function.h"
+using namespace std;
+
+void header(){
+	cout<<"===================================================="<<endl;
+	cout<<"menghitung panjang sisi menggunakan konsep pytagoras"<<endl;
+	cout<<"===================================================="<<endl;
+}
+
+int main()
+{
+system("cls");
+segitiga tiga;
+
+bangun *list[1];
+list[0]=&tiga;
+
+	int pilih;
+	char ulang;
+	do{
+		system("cls");
+		  header();
+		  cout<<"1) segitiga siku-siku\n";
+		  cout<<"2) segitiga lancip\n";
+		  cout<<"3) segitiga tumpul\n";
+		  cout<<"masukkan sudut:";
+		  cin>>pilih;
+		  
+		  if(pilih==90){
+				  cout<<"Segitiga Siku-siku."<<endl;
+		          list[0]->get_data();
+		          list[0]->display_area();
+		          getch();
+		          }
+		  else if ((pilih>=0)&&(pilih<=89)){
+		  		  cout<<"Segitiga Lancip."<<endl;
+		          list[0]->get_data();
+		          list[0]->display_area();
+		          getch();
+		          }
+		  else if ((pilih>=91)&&(pilih<=179)){
+		  		  cout<<"Segitiga Tumpul."<<endl;
+		          list[0]->get_data();
+		          list[0]->display_area();
+		          getch();
+		          }
+		 else{
+		     cout <<"maaf sudut yang anda masukkan salah"<<endl;
+		     getch();
+			}
+		cout << "apakaah ingin menghitung lagi ? "<<endl ;
+		cout << "Y/T"<<endl;
+		cout << " pilih : ";
+		cin >> ulang;	
+		system("cls");
+	}while(ulang=='Y'||ulang=='y');
+	cout << "Terima kasih telah menggunakan program saya untuk menghitung"<<endl<<endl;
+	getch ();
+	cout << "=============================="<<endl;	
+	cout << "Tatag Indah Larasati"<<endl;
+	cout << " D04217025"<<endl;
+    cout << "=============================="<<endl;	
+    return 0;
+}
